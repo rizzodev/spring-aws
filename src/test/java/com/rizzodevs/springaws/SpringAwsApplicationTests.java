f765cd5edd849eb8e99e5d769a51c72ba0207a72
rizzoirfan <rizzo.irfan@knowledgetouch.com>
2022-07-24 15:05:58 +0700

    starter spring

---
+++ src/test/java/com/rizzodevs/springaws/SpringAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rizzodevs.springaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
